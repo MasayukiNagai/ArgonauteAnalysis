b6e2b3922852e5484dfe8bc177720b7ebec9f4aa
MasayukiNagai <moon.masayukinagai@gmail.com>
2024-03-07 14:45:52 -0500

    Organize code

---
+++ VAE_MSA.py
@@ -0,0 +1,175 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pytorch_lightning as pl
+
+
+activation_dict = {
+    "ReLU": nn.ReLU,
+    "LeakyReLU": nn.LeakyReLU,
+    "SELU": nn.SELU,
+    "ELU": nn.ELU,
+}
+
+
+class VAE(pl.LightningModule):
+    def __init__(self, input_length, num_classes, device="cpu", h_params=None):
+        super().__init__()
+        self.input_length = input_length
+        self.num_classes = num_classes
+        self._device = device
+        self.h_params = {
+            "z_dim": 2,
+            "lr": 1e-3,
+            "dropout": 0.1,
+            "beta": 0.01,
+            "activation": "LeakyReLU",
+            "dim1": 1500,
+            "dim2": 1000,
+            "is_annealing": True,
+        }
+        if h_params is not None:
+            self.h_params.update(h_params)
+            if self.h_params["dim1"] < self.h_params["dim2"]:  ### Remove this?
+                self.h_params["dim1"], self.h_params["dim2"] = self.h_params["dim2"], self.h_params["dim1"]
+        self.save_hyperparameters(self.h_params)
+        self.max_epochs = 20  ### Incorporate this into h_params only if is_annealing = True
+
+        encoder_dims = [self.h_params["dim1"], self.h_params["dim2"]]
+        decoder_dims = [self.h_params["dim2"], self.h_params["dim1"]]
+        z_dim = self.h_params["z_dim"]
+        activation = activation_dict[self.h_params["activation"]]
+
+        # Build Encoder
+        modules = []
+        input_dim = self.input_length * self.num_classes
+        in_dim = input_dim
+        for h_dim in encoder_dims:
+            modules.append(
+                nn.Sequential(
+                    nn.Linear(in_dim, h_dim),
+                    nn.Dropout(self.h_params["dropout"]),
+                    nn.BatchNorm1d(h_dim),
+                    activation(),
+                )
+            )
+            in_dim = h_dim
+
+        self.encoder = nn.Sequential(*modules)
+        self.encoder_mu = nn.Linear(encoder_dims[-1], z_dim)
+        self.encoder_log_var = nn.Linear(encoder_dims[-1], z_dim)
+
+        # Build Decoder
+        modules = []
+        in_dim = z_dim
+        for h_dim in decoder_dims:
+            modules.append(
+                nn.Sequential(
+                    nn.Linear(in_dim, h_dim),
+                    nn.Dropout(self.h_params["dropout"]),
+                    nn.BatchNorm1d(h_dim),
+                    activation(),
+                )
+            )
+            in_dim = h_dim
+        modules.append(nn.Sequential(nn.Linear(decoder_dims[-1], input_dim)))
+        self.decoder = nn.Sequential(*modules)
+
+        self.loss_fn = nn.CrossEntropyLoss(reduction="mean")
+        self.eval_loss = []
+
+    def encode(self, x):  # x: [N, L]
+        x_one_hot = F.one_hot(x.long(), self.num_classes).float()  # [N, L, C]
+        x_one_hot_flat = x_one_hot.reshape(x_one_hot.shape[0], -1)  # [N, L*C]
+        h = self.encoder(x_one_hot_flat)
+        mu = self.encoder_mu(h)
+        log_var = self.encoder_log_var(h)
+        return mu, log_var
+
+    def decode(self, z):
+        h = self.decoder(z)  # [N, L*C]
+        N = h.shape[0]
+        L = self.input_length
+        C = self.num_classes
+        h_reshaped = h.view(N, L, C)  # [N, L, C]
+        h_softmax = F.softmax(h_reshaped, dim=2)
+        h_final = h_softmax.view(N, L * C)  # [N, L*C]
+        return h_final
+
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def forward(self, x, n_samples=1):
+        mu, log_var = self.encode(x)
+        z_samples = self.reparameterize(mu, log_var)
+        x_recon = self.decode(z_samples)
+        return x_recon, mu, log_var
+
+    def _step(self, batch, batch_idx, is_training=True):
+        x = batch.long().to(self._device)  # [N, L]
+        x_recon, mu, log_var = self(x)  # x_recon: [N, L*C]
+        N, L = x.shape
+        C = self.num_classes
+        x_recon = x_recon.view(N, L, C)
+        x = x.view(-1)  # [N*L]
+        x_recon = x_recon.view(-1, C)  # [N*L, C]
+        x_recon_logigts = torch.log(x_recon + 1e-6)
+        recon_loss = self.loss_fn(x_recon_logigts, x)
+        kl_loss = torch.mean(
+            -0.5 * torch.sum(1 + log_var - mu**2 - log_var.exp(), dim=1), dim=0
+        )
+        if is_training and self.hparams["is_annealing"]:
+            beta = self.h_params["beta"] * self.current_epoch / self.max_epochs
+        else:
+            beta = self.h_params["beta"]
+        loss = recon_loss + beta * kl_loss
+        return loss, recon_loss, kl_loss
+
+    def training_step(self, batch, batch_idx):
+        loss, recon_loss, kl_loss = self._step(batch, batch_idx)
+        self.log_dict(
+            {"train_loss": loss, "recon_loss": recon_loss, "kl_loss": kl_loss},
+            on_step=False,
+            on_epoch=True,
+        )
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss, recon_loss, kl_loss = self._step(batch, batch_idx, is_training=False)
+        self.log_dict({"val_loss": loss}, on_step=False, on_epoch=True)
+        self.eval_loss.append(loss)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        loss, recon_loss, kl_loss = self._step(batch, batch_idx, is_training=False)
+        self.log_dict({"test_loss": loss}, on_step=False, on_epoch=True)
+
+        return loss
+
+    def on_validation_epoch_end(self):
+        avg_loss = torch.stack(self.eval_loss).mean()
+        self.log("ptl/val_loss", avg_loss, sync_dist=True)
+        self.eval_loss.clear()
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.h_params["lr"])
+
+
+def test():
+    N = 10
+    L = 50
+    C = 21
+    x = torch.randint(low=0, high=C, size=(N, L))
+    test_model = VAE(input_length=L, num_classes=C)
+    x_recon, mu, log_var = test_model(x)
+    print(f"N: {N}, L: {L}, C: {C}")
+    print(f"x: {x.shape}")
+    print(f"x_recon: {x_recon.shape}")
+    print(f"mu: {mu.shape}")
+    print(f"log_var: {log_var.shape}")
+
+
+if __name__ == "__main__":
+    test()
--- pssm_analysis.py
+++ pssm_analysis.py
@@ -13,7 +13,15 @@ aa1_to_index = {'A': 0, 'C': 1, 'D': 2, 'E': 3, 'F': 4, 'G': 5, 'H': 6,
                 'Q': 13, 'R': 14, 'S': 15, 'T': 16, 'V': 17, 'W': 18,
                 'Y': 19, 'X':20, 'Z': 21, '-': 22, 'B': 23}
 # AA_ALPHABETS = "ACDEFGHIKLMNPQRSTVWY-"
-
+color_dict = {
+    'DEC': [.42, .16, .42],
+    'PG': [.47, .47, 0.0],
+    'MIWALFV': [.13, .35, .61],
+    'NTSQ': [.25, .73, .28],
+    'RK': [.74, .18, .12],
+    'HY': [.09, .47, .46],
+    '-': [0, 0, 0],
+}
 
 def calc_pssm_from_msa(msa, num_classes):
     msa2onehot = encode_msa2onehot(msa, num_classes)
@@ -23,18 +31,22 @@ def calc_pssm_from_msa(msa, num_classes):
 
 
 def encode_msa2onehot(path, num_classes):
-    with open(path, 'r') as f:
-        alignment = AlignIO.read(f, 'fasta')
+    with open(path, "r") as f:
+        alignment = AlignIO.read(f, "fasta")
     encoded_sequences = {}
     for record in alignment:
-        x_numeric = numeric_encode(record.seq, max_index=num_classes-1)
-        x_one_hot = nn.functional.one_hot(x_numeric.long(), num_classes).float() # [N, L, C]
+        x_numeric = numeric_encode(record.seq, max_index=num_classes - 1)
+        x_one_hot = nn.functional.one_hot(
+            x_numeric.long(), num_classes
+        ).float()  # [N, L, C]
         encoded_sequences[record.id] = x_one_hot
     return encoded_sequences
 
 
 def numeric_encode(seq, max_index=None):
-    seq_numeric = np.array([aa1_to_index[aa] for aa in str(seq).upper().replace('.', '-')])
+    seq_numeric = np.array(
+        [aa1_to_index[aa] for aa in str(seq).upper().replace(".", "-")]
+    )
     if max_index is not None:
         seq_numeric[seq_numeric > max_index] = max_index
     return torch.from_numpy(seq_numeric)
@@ -53,8 +65,8 @@ def plot_pssm(pssm, ALPHABETS, title=None):
     ax = sns.heatmap(pssm.T)
     if title is not None:
         plt.title(title)
-    plt.xlabel('Position in Sequence')
-    plt.ylabel('Amino Acid')
+    plt.xlabel("Position in Sequence")
+    plt.ylabel("Amino Acid")
     ax.set_yticks(np.arange(len(ALPHABETS)) + 0.5)
     ax.set_yticklabels(list(ALPHABETS), rotation=0)
 
@@ -68,55 +80,29 @@ def calc_information_content(pssm_df, background_freq):
         ic = p_i.apply(lambda x: x * np.log2(x / q_i) if x > 0 else 0)
         ic_df[column] = ic
     # Sum across rows to get the total information content per position
-    ic_df['Total IC'] = ic_df.sum(axis=1)
+    ic_df["Total IC"] = ic_df.sum(axis=1)
     return ic_df
 
 
 def plot_logomaker(pssm_df, length_per_row=100, title=None):
     L = length_per_row
     num_rows = len(pssm_df) // L + 1
-    
-    fig, axs = plt.subplots(num_rows, 1, figsize=(24, 2*num_rows))
-    
+
+    fig, axs = plt.subplots(num_rows, 1, figsize=(24, 2 * num_rows))
+
     ylim = pssm_df.max().max()  # not ideal for probability
-    
+
     for i in range(num_rows):
         logo = logomaker.Logo(
-            pssm_df.iloc[(i)*L:(i+1)*L, :],         
-            color_scheme='NajafabadiEtAl2017',
-            vpad=.1, width=.8, ax=axs[i])
+            pssm_df.iloc[(i) * L : (i + 1) * L, :],
+            color_scheme=color_dict,
+            vpad=0.1,
+            width=0.8,
+            ax=axs[i],
+        )
         logo.style_xticks(anchor=0, spacing=5, rotation=45)
         logo.ax.set_ylim([0, ylim])
-    
+
     plt.tight_layout(rect=[0, 0.03, 1, 0.95])
     if title is not None:
-        plt.suptitle(f'{title}', fontsize=18)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        plt.suptitle(f"{title}", fontsize=18)
